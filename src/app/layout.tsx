726a905a6bf7b3d1d7602360a22bb9d19d9418e5
ls-devs <58638832+ls-devs@users.noreply.github.com>
2023-05-23 10:50:27 +0200

    feat: Add Navbar & Footer
    Navbar & Footer Components responsive
    Dynamic Footer
    Change prettier & tailwindcss config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,26 @@
-import QueryProvider from "@/utils/queryProvider"
-import "./globals.scss"
-import {Inter} from "next/font/google"
+import QueryProvider from "@/utils/queryProvider";
+import "./globals.scss";
+import {Inter} from "next/font/google";
+import {Navbar} from "@/components/navbar";
+import {Footer} from "@/components/footer";
 
-const inter = Inter({subsets: ["latin"]})
+const inter = Inter({subsets: ["latin"]});
 
 export const metadata = {
   title: "Chez Carmen",
   description: "Restaurant des Abattoirs",
-}
+};
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="fr">
       <body className={inter.className}>
-        <QueryProvider>{children}</QueryProvider>
+        <QueryProvider>
+          <Navbar />
+          {children}
+          <Footer />
+        </QueryProvider>
       </body>
     </html>
-  )
+  );
 }
