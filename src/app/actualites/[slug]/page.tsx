03de5b301100abb836caf434f07de57afe4af1f8
ls-devs <58638832+ls-devs@users.noreply.github.com>
2023-09-07 14:47:41 +0200

    add myactu

--- src/app/actualites/[slug]/page.tsx
+++ src/app/actualites/[slug]/page.tsx
@@ -1,12 +1,40 @@
-import { useQueryUtils } from "@/hooks/useQueryUtils";
-import { IActualites } from "@/types/types";
-import { fetchActualites } from "@/utils/fetchs/fetchs";
+'use client';
+import { useQueryUtils } from '@/hooks/useQueryUtils';
+import { IActualites, MyActualite } from '@/types/types';
+import { fetchActualites } from '@/utils/fetchs/fetchs';
+import Image from 'next/image';
+import { useState } from 'react';
 
 export default function Page({ params }: { params: { slug: string } }) {
+  const [myActu, setMyActu] = useState<IActualites>();
+  const unslugify = (slug: string) =>
+    slug
+      .replace(/\-/g, ' ')
+      .replace(
+        /\w\S*/g,
+        (text) => text.charAt(0).toUpperCase() + text.slice(1).toLowerCase()
+      );
   const { data } = useQueryUtils<IActualites[]>({
     qKey: ['getActualites'],
     qFn: () => fetchActualites(),
   });
 
-  return <div>My Post: {params.slug}</div>;
+  data?.forEach((actu) => {
+    if (actu.acf.title === unslugify(actu.acf.title)) {
+      setMyActu(actu);
+    }
+  });
+
+  return (
+    <div>
+      {myActu?.acf.image && (
+        <Image
+          src={myActu?.acf.image.toString()}
+          alt={`${myActu?.acf.title}`}
+          width={100}
+          height={100}
+        />
+      )}
+    </div>
+  );
 }
