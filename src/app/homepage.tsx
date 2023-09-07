f4d53ed0839a57b823b84b931bc8b7b22f58d3f7
ls-devs <58638832+ls-devs@users.noreply.github.com>
2023-09-07 17:22:10 +0200

    fix video

--- src/app/homepage.tsx
+++ src/app/homepage.tsx
@@ -284,7 +284,7 @@ export const HomePage = () => {
                 muted
                 poster="/img/home/heading/heading_video_thumbnail.png"
                 className="relative h-[300px] w-full bg-cover bg-center bg-no-repeat object-cover px-5 lg:h-[450px] xl:h-[650px]"
-                src={`{dataV?.[0].acf.url}`}
+                src={`${dataV?.[0].acf.url}`}
                 playsInline
               ></video>
               <h3 className="absolute -top-4 left-0 -rotate-6 font-softgank text-2xl text-cream-carmen sm:-top-16 sm:left-36 sm:z-10 sm:rotate-6 sm:text-4xl">
